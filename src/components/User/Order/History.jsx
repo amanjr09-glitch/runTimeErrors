ad92416a8b86ef9e016c4251ff810cbf691646f3
Shoto_Itadori <atsyt09@gmail.com>
2023-10-08 03:32:28 +0530

    integration done

--- src/components/User/Order/History.jsx
+++ src/components/User/Order/History.jsx
@@ -4,9 +4,12 @@ import HistoryCard2 from './HistoryCard2'
 
 function History(){
     return(
-        <div className='p-20 bg-slate-100 backdrop-blur-lg'>
-            <div className=' p-5 text-3xl font-semibold px-10'>History</div>
-            <div>
+        <div className='p-4 bg-slate-100 backdrop-blur-lg'>
+            <div className=' p-5 text-3xl font-semibold px-10 uppercase'>History</div>
+            <hr/>
+            <div className='overflow-y-auto max-h-screen mb-2'>
+                <HistoryCard2/>
+                <HistoryCard2/>
                 <HistoryCard2/>
                 <HistoryCard2/>
                 <HistoryCard2/>
