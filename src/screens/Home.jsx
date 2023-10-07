01f1642e400d388695ae7be4c27cafac38d3915f
suraj kumar <surajsinhakingr77>
2023-10-07 19:19:31 +0530

    suraj done

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // import Login from '../Components/Auth/Login';
 
-import JobCard from '../Components/Job/JobCard';
+// import JobCard from '../Components/Job/JobCard';
 
 function Home() {
   return (
     <div className='text-xl text-black'>
       {/* <Login/> */}
 
-      <JobCard/>
+      {/* <JobCard/> */}
     </div>
   )
 }
